5173a08dbb64ee614c01b2d180da34a39ced071d
Steeve Luis Angel Torres Agustin <iam@steeve.me>
2018-11-01 23:26:53 -0500

    Proyecto Base con dependencias en composer

---
+++ generated-sql/default.sql
@@ -0,0 +1,82 @@
+
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ---------------------------------------------------------------------
+-- account
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `account`;
+
+CREATE TABLE `account`
+(
+    `id` INTEGER NOT NULL,
+    `user_id` INTEGER NOT NULL,
+    `name` VARCHAR(60),
+    PRIMARY KEY (`id`),
+    INDEX `user_id` (`user_id`),
+    CONSTRAINT `account_ibfk_1`
+        FOREIGN KEY (`user_id`)
+        REFERENCES `user` (`id`)
+        ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+-- ---------------------------------------------------------------------
+-- category
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `category`;
+
+CREATE TABLE `category`
+(
+    `id` INTEGER NOT NULL,
+    `name` VARCHAR(60),
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+-- ---------------------------------------------------------------------
+-- transaction
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `transaction`;
+
+CREATE TABLE `transaction`
+(
+    `id` INTEGER NOT NULL,
+    `account_id` INTEGER NOT NULL,
+    `category_id` INTEGER NOT NULL,
+    `description` TEXT,
+    `date` DATE,
+    `amount` DOUBLE,
+    PRIMARY KEY (`id`),
+    INDEX `category_id` (`category_id`),
+    INDEX `account_id` (`account_id`),
+    CONSTRAINT `transaction_ibfk_1`
+        FOREIGN KEY (`category_id`)
+        REFERENCES `category` (`id`)
+        ON UPDATE CASCADE,
+    CONSTRAINT `transaction_ibfk_2`
+        FOREIGN KEY (`account_id`)
+        REFERENCES `account` (`id`)
+        ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+-- ---------------------------------------------------------------------
+-- user
+-- ---------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user`
+(
+    `id` INTEGER NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `displayName` VARCHAR(255),
+    `password` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
---
+++ propel.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'propel' => [
+        'database' => [
+            'connections' => [
+                'default' => [
+                    'adapter' => 'mysql',
+                    'dsn' => 'mysql:host=localhost;port=3306;dbname=money',
+                    'user' => 'steeve',
+                    'password' => 'Mishibebe69',
+                    'settings' => [
+                        'charset' => 'utf8'
+                    ]
+                ]
+            ]
+        ]
+    ]
+];
